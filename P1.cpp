92320f45ef873a53388a213d7a404f3a1726432c
manasthakur <manasthakur17@gmail.com>
2014-03-15 00:58:17 +0530

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -4,19 +4,19 @@
 #include<pthread.h>
 #define NUM_STUDENTS 25
 
-int ticketQueue[250];											// ticketQueue for 25 students, with max 10 questions per student
-int front;														// this tells who is the student currently being answered
-int rear;														// this adds up each new question of student to the end (ensures fairness)
-int curr_qid;													// question# being answered currently
-int curr_sid;													// student id# being answered currently
-bool workDone = false;											// set when all students have finished; to terminate the professor
-pthread_mutex_t rearMutex = PTHREAD_MUTEX_INITIALIZER;			// mutex to allot next ticket to thread
-pthread_mutex_t checkQueueMutex = PTHREAD_MUTEX_INITIALIZER;		// mutex to check if queue is empty
-pthread_mutex_t questionMutex = PTHREAD_MUTEX_INITIALIZER;		// mutex used with cond: questionStartDone
-pthread_mutex_t answerMutex = PTHREAD_MUTEX_INITIALIZER;			// mutex used with cond: answerDone
-pthread_mutex_t currMutex = PTHREAD_MUTEX_INITIALIZER;			// mutex used while updating the variables curr_qid and curr_sid
-pthread_cond_t questionStartDone = PTHREAD_COND_INITIALIZER;		// signal from QuestionStart() to AnswerStart()
-pthread_cond_t answerDone = PTHREAD_COND_INITIALIZER;			// signal from AnswerDone() to QuestionDone()
+int ticketQueue[250];		// ticketQueue for 25 students, with max 10 questions per student
+int front;			// this tells who is the student currently being answered
+int rear;			// this adds up each new question of student to the end (ensures fairness)
+int curr_qid;			// question# being answered currently
+int curr_sid;			// student id# being answered currently
+bool workDone = false;		// set when all students have finished; to terminate the professor
+pthread_mutex_t rearMutex = PTHREAD_MUTEX_INITIALIZER;		// mutex to allot next ticket to thread
+pthread_mutex_t checkQueueMutex = PTHREAD_MUTEX_INITIALIZER;	// mutex to check if queue is empty
+pthread_mutex_t questionMutex = PTHREAD_MUTEX_INITIALIZER;	// mutex used with cond: questionStartDone
+pthread_mutex_t answerMutex = PTHREAD_MUTEX_INITIALIZER;	// mutex used with cond: answerDone
+pthread_mutex_t currMutex = PTHREAD_MUTEX_INITIALIZER;		// mutex used while updating the variables curr_qid and curr_sid
+pthread_cond_t questionStartDone = PTHREAD_COND_INITIALIZER;	// signal from QuestionStart() to AnswerStart()
+pthread_cond_t answerDone = PTHREAD_COND_INITIALIZER;		// signal from AnswerDone() to QuestionDone()
 
 
 /**
@@ -42,8 +42,8 @@ void AnswerStart() {
 	pthread_mutex_lock(&questionMutex);
 	pthread_cond_wait(&questionStartDone, &questionMutex);
 	pthread_mutex_unlock(&questionMutex);
-	int toSleep = rand() % 14 + 2;				// given that professor takes 2-15 time units to answer the question
-	usleep(toSleep * 1000);						// obtain corresponding microseconds
+	int toSleep = rand() % 14 + 2;		// given that professor takes 2-15 time units to answer the question
+	usleep(toSleep * 1000);			// obtain corresponding microseconds
 }
 
 /**
@@ -88,7 +88,7 @@ void QuestionStart(long sid, int i) {
 	curr_sid = (int) sid;
 	curr_qid = i;
 	pthread_mutex_unlock(&currMutex);
-	usleep(1000);						// time to ask the question (assumed: 1 time unit = 1000 microseconds)
+	usleep(1000);				// time to ask the question (assumed: 1 time unit = 1000 microseconds)
 }
 
 /**
@@ -132,8 +132,8 @@ int main(int argc, char **argv) {
 	
 	front = rear = -1;
 	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);				// make students joinable, so that we can get the
-																			// condition to terminate later
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);  // make students joinable, so that we can get the
+									// condition to terminate later
 	rc = pthread_create(&professor, NULL, TheProfessor, (void *)NULL);
 	if(rc) {
 		printf("Could not create professor thread. Error code: %d\n", rc);
@@ -156,7 +156,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    workDone = true;									// indicates that all students are done, so professor can go home now
+    workDone = true;			// indicates that all students are done, so professor can go home now
     pthread_attr_destroy(&attr);
 	pthread_mutex_destroy(&rearMutex);
 	pthread_mutex_destroy(&checkQueueMutex);
