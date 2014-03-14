2d51251d9e55d6943cd7fa02718de5b29cdc4df7
manasthakur <manasthakur17@gmail.com>
2014-03-15 00:55:50 +0530

    Update P1.cpp
    Given detailed comments

--- P1.cpp
+++ P1.cpp
@@ -4,20 +4,24 @@
 #include<pthread.h>
 #define NUM_STUDENTS 25
 
-int ticketQueue[250];
-int front;
-int rear;
-int curr_qid;
-int curr_sid;
-bool workDone = false;
-pthread_mutex_t rearMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t checkQueueMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t questionMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t answerMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t currMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t questionStartDone = PTHREAD_COND_INITIALIZER;
-pthread_cond_t answerDone = PTHREAD_COND_INITIALIZER;
+int ticketQueue[250];											// ticketQueue for 25 students, with max 10 questions per student
+int front;														// this tells who is the student currently being answered
+int rear;														// this adds up each new question of student to the end (ensures fairness)
+int curr_qid;													// question# being answered currently
+int curr_sid;													// student id# being answered currently
+bool workDone = false;											// set when all students have finished; to terminate the professor
+pthread_mutex_t rearMutex = PTHREAD_MUTEX_INITIALIZER;			// mutex to allot next ticket to thread
+pthread_mutex_t checkQueueMutex = PTHREAD_MUTEX_INITIALIZER;		// mutex to check if queue is empty
+pthread_mutex_t questionMutex = PTHREAD_MUTEX_INITIALIZER;		// mutex used with cond: questionStartDone
+pthread_mutex_t answerMutex = PTHREAD_MUTEX_INITIALIZER;			// mutex used with cond: answerDone
+pthread_mutex_t currMutex = PTHREAD_MUTEX_INITIALIZER;			// mutex used while updating the variables curr_qid and curr_sid
+pthread_cond_t questionStartDone = PTHREAD_COND_INITIALIZER;		// signal from QuestionStart() to AnswerStart()
+pthread_cond_t answerDone = PTHREAD_COND_INITIALIZER;			// signal from AnswerDone() to QuestionDone()
 
+
+/**
+ * Returns true if the queue is empty
+ */
 bool isTicketQueueEmpty() {
 	pthread_mutex_lock(&checkQueueMutex);
 	if(front == rear) {
@@ -30,14 +34,22 @@ bool isTicketQueueEmpty() {
 	}
 }
 
+/**
+ * Waits for QuestionStart() to finish,
+ * as the professor feels it to be rude
+ */
 void AnswerStart() {
 	pthread_mutex_lock(&questionMutex);
 	pthread_cond_wait(&questionStartDone, &questionMutex);
 	pthread_mutex_unlock(&questionMutex);
-	int toSleep = rand() % 14 + 2;
-	usleep(toSleep * 1000);
+	int toSleep = rand() % 14 + 2;				// given that professor takes 2-15 time units to answer the question
+	usleep(toSleep * 1000);						// obtain corresponding microseconds
 }
 
+/**
+ * Signals QuestionDone() that professor has finished giving the answer,
+ * so student can either go or ask next question (max 10 though)
+ */
 void AnswerDone() {
 	pthread_mutex_lock(&answerMutex);
 	printf("Professor is done answering question #%d of student #%d\n", curr_qid, curr_sid);
@@ -45,6 +57,9 @@ void AnswerDone() {
 	pthread_mutex_unlock(&answerMutex);
 }
 
+/**
+ * This is the only Professor thread
+ */
 void *TheProfessor(void *nothing) {
 	while(!workDone) {
 		if(!isTicketQueueEmpty()) {
@@ -58,6 +73,8 @@ void *TheProfessor(void *nothing) {
 	}
 }
 
+/** Spins while myTicketNum is not same as currently being answered (ensures no-starvation)
+ */
 void QuestionStart(long sid, int i) {
 	printf("Student #%ld is ready to ask question #%d\n", sid, i);
 	
@@ -71,22 +88,25 @@ void QuestionStart(long sid, int i) {
 	curr_sid = (int) sid;
 	curr_qid = i;
 	pthread_mutex_unlock(&currMutex);
-	usleep(1000);
+	usleep(1000);						// time to ask the question (assumed: 1 time unit = 1000 microseconds)
 }
 
+/**
+ * Waits for signal from AnswerDone()
+ */
 void QuestionDone(long sid, int i) {
 	pthread_mutex_lock(&answerMutex);
-	//printf("\nhehe\n");
 	pthread_cond_wait(&answerDone, &answerMutex);
-	//printf("\nhehe\n");
 	pthread_mutex_unlock(&answerMutex);
 	printf("Student #%ld has got the answer for question #%d\n", sid, i);
 }
 
+/**
+ * This is the common student method
+ */
 void *OneStudent(void *studentid) {
 	int numQuestions = rand() % 10 + 1;
 	long sid = (long)studentid;
-	//printf("Student %ld will ask %d questions\n", sid, numQuestions);
 
 	for(int i = 0; i < numQuestions; ++i) {
 		QuestionStart(sid, i);
@@ -100,6 +120,9 @@ void *OneStudent(void *studentid) {
 	}
 }
 
+/**
+ * Execution starts from here
+ */
 int main(int argc, char **argv) {
 	pthread_t professor;
 	pthread_t student[NUM_STUDENTS];
@@ -109,8 +132,8 @@ int main(int argc, char **argv) {
 	
 	front = rear = -1;
 	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);				// make students joinable, so that we can get the
+																			// condition to terminate later
 	rc = pthread_create(&professor, NULL, TheProfessor, (void *)NULL);
 	if(rc) {
 		printf("Could not create professor thread. Error code: %d\n", rc);
@@ -133,7 +156,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    workDone = true;
+    workDone = true;									// indicates that all students are done, so professor can go home now
     pthread_attr_destroy(&attr);
 	pthread_mutex_destroy(&rearMutex);
 	pthread_mutex_destroy(&checkQueueMutex);
